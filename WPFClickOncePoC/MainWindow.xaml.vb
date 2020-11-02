545b6eca8258202340eda439edba8155fd24d03c
priyanka7891 <tiwpriyashukl7@gmail.com>
2020-11-02 19:08:33 +0530

    My Very fifth Commit

--- WPFClickOncePoC/MainWindow.xaml.vb
+++ WPFClickOncePoC/MainWindow.xaml.vb
@@ -1,5 +1,5 @@
 ﻿Class MainWindow
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        MessageBox.Show("Hi Click Once PoC. next version 2")
+        MessageBox.Show("Hi Click Once PoC. next version 4")
     End Sub
 End Class
